abe5afddfb9e5bf7d12c2479dab8a47c2b757e1e
Toma Rosa <toma.rosa@protonmail.com>
2023-02-11 18:18:52 +0100

    feat: auth form

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -1,9 +1,21 @@
-"use client"
+"use client";
+
+import { AuthForm } from "@/components/auth-form";
+import { Command } from "lucide-react";
 
 export default function SigninPage() {
   return (
-    <div>
-      signin
+    <div className="grid place-items-center h-screen px-6">
+      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+        <div className="flex flex-col space-y-2 text-center">
+          <Command className="mx-auto h-6 w-6" />
+          <h1 className="text-2xl font-bold">Welcome back</h1>
+          <p className="text-sm text-slate-600">
+            Enter your email to sign in to your account
+          </p>
+        </div>
+        <AuthForm />
+      </div>
     </div>
   );
 }
---
+++ components/auth-form.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import * as React from "react";
+import * as z from "zod";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useSearchParams } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { signIn } from "next-auth/react";
+import { Loader2 } from "lucide-react";
+
+import { userAuthSchema } from "@/lib/validations/auth";
+import { useToast } from "@/hooks/use-toast";
+import { cn } from "@/lib/utils";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { Label } from "@radix-ui/react-label";
+
+interface AuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+type FormData = z.infer<typeof userAuthSchema>;
+
+export function AuthForm({ className, ...props }: AuthFormProps) {
+  const { toast } = useToast();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(userAuthSchema),
+  });
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const searchParams = useSearchParams();
+
+  async function onSubmit(data: FormData) {
+    setIsLoading(true);
+
+    const signInResult = await signIn("email", {
+      email: data.email.toLowerCase(),
+      redirect: false,
+      callbackUrl: searchParams.get("from") || "/dashboard",
+    });
+
+    setIsLoading(false);
+
+    if (!signInResult?.ok) {
+      return toast({
+        title: "Something went wrong.",
+        description: "Your sign in request failed. Please try again.",
+      });
+    }
+
+    return toast({
+      title: "Check your email",
+      description: "We sent you a login link. Be sure to check your spam too.",
+    });
+  }
+
+  return (
+    <div className={cn("grid gap-6", className)} {...props}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="grid gap-2">
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="email">
+              Email
+            </Label>
+            <Input
+              id="email"
+              placeholder="name@example.com"
+              type="email"
+              autoCapitalize="none"
+              autoComplete="email"
+              autoCorrect="off"
+              disabled={isLoading}
+              {...register("email")}
+            />
+            {errors?.email && (
+              <p className="px-1 text-xs text-red-600">
+                {errors.email.message}
+              </p>
+            )}
+          </div>
+          <Button disabled={isLoading}>
+            {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+            Sign In with Email
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+}
