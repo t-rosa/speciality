026e57c2d42d49c663697289e1a893f4bc559860
Toma Rosa <toma.rosa@protonmail.com>
2023-03-05 10:35:05 +0100

    feat: fetch and display project by user

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,3 +1,43 @@
+import { authOptions } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { getCurrentUser } from "@/lib/session";
+import { User } from "@prisma/client";
+import { redirect } from "next/navigation"
+import { cache } from "react";
+
+export const metadata = {
+  title: "Dashboard",
+}
+
+const getProjectsByUser = cache(async function(userId: User["id"]) {
+  return await db.project.findMany({
+    where: {
+      ownerId: userId,
+    },
+    select: {
+      id: true,
+      createdAt: true,
+      name: true,
+      description: true,
+      due: true
+    },
+    orderBy: {
+      updatedAt: "desc",
+    },
+  })
+})
+
 export default async function DashboardPage() {
-  return <div>bonjour</div>;
+  const user = await getCurrentUser()
+
+  if (!user) {
+    redirect(authOptions?.pages?.signIn || "/signin")
+  }
+
+  const projects = await getProjectsByUser(user.id)
+
+  return <div>
+    {
+      projects.map((project) => (<div key={project.id}>{project.name}</div>))
+    }</div>;
 }
