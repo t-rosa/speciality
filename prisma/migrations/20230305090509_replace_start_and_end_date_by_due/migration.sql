9d693c17e506289e3c100223e14e33302f336520
Toma Rosa <toma.rosa@protonmail.com>
2023-03-05 10:07:02 +0100

    chore: update date in prisma schema and add seed script

---
+++ prisma/migrations/20230305090509_replace_start_and_end_date_by_due/migration.sql
@@ -0,0 +1,18 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `endDate` on the `Project` table. All the data in the column will be lost.
+  - You are about to drop the column `startDate` on the `Project` table. All the data in the column will be lost.
+  - You are about to drop the column `endDate` on the `Task` table. All the data in the column will be lost.
+  - You are about to drop the column `startDate` on the `Task` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Project" DROP COLUMN "endDate",
+DROP COLUMN "startDate",
+ADD COLUMN     "due" TIMESTAMP(3);
+
+-- AlterTable
+ALTER TABLE "Task" DROP COLUMN "endDate",
+DROP COLUMN "startDate",
+ADD COLUMN     "due" TIMESTAMP(3);
